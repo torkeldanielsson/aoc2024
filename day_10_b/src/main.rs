df78fc9f428ef66ed101c685784f1622fd20bdf8
torkel <torkel@voysys.se>
2024-12-10 10:08:08 +0100

    10a

---
+++ day_10_b/src/main.rs
@@ -0,0 +1,63 @@
+use glam::{ivec2, IVec2};
+use std::{error::Error, time::Instant};
+
+struct Map<'a> {
+    data: &'a [u8],
+    w: i32,
+    h: i32,
+}
+
+impl<'a> Map<'a> {
+    fn new(data: &'a str) -> Self {
+        let w = data.find('\n').unwrap() as i32;
+        let h = data.len() as i32 / w - 1;
+
+        Map {
+            data: data.as_bytes(),
+            w,
+            h,
+        }
+    }
+
+    fn at(&self, coord: IVec2) -> u8 {
+        if coord.x < 0 || coord.x >= self.w || coord.y < 0 || coord.y >= self.h {
+            return 255;
+        }
+        self.data[(coord.y * (self.w + 1) + coord.x) as usize] - 48
+    }
+}
+
+fn is_path(map: &Map, pos: IVec2, next_level: u8, acc_res: &mut i32) {
+    if next_level == 10 {
+        *acc_res += 1;
+        return;
+    }
+
+    for dir in [ivec2(-1, 0), ivec2(0, -1), ivec2(1, 0), ivec2(0, 1)] {
+        if map.at(pos + dir) == next_level {
+            is_path(map, pos + dir, next_level + 1, acc_res)
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let t = Instant::now();
+
+    let input = include_str!("../input");
+
+    let map = Map::new(input);
+
+    let mut res = 0;
+
+    for y in 0..map.h {
+        for x in 0..map.w {
+            if map.at(ivec2(x, y)) == 0 {
+                is_path(&map, ivec2(x, y), 1, &mut res);
+            }
+        }
+    }
+
+    println!("res: {res}, {} us", t.elapsed().as_micros());
+
+    Ok(())
+}
