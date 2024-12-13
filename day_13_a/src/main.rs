fa4bbb3fa5cbdf0e89cd5746499a2f73caae3281
torkel <torkel@voysys.se>
2024-12-13 09:26:19 +0100

    wip

---
+++ day_13_a/src/main.rs
@@ -0,0 +1,10 @@
+use std::{error::Error, time::Instant};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let t = Instant::now();
+
+    let input = include_str!("../test");
+    
+    
+    Ok(())
+}
