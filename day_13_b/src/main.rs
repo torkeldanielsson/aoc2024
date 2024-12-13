38c8fdf66a9409a212070664f7c12fc6c31fc56a
torkel <torkel@voysys.se>
2024-12-13 16:16:26 +0100

    fy

--- day_13_b/src/main.rs
+++ day_13_b/src/main.rs
@@ -2,28 +2,6 @@ use std::{error::Error, time::Instant};
 
 use glam::i64vec2;
 
-fn update_step(a: &mut i32, old_a: &mut i32, quotient: i32) {
-    let temp = *a;
-    *a = *old_a - quotient * temp;
-    *old_a = temp;
-}
-
-pub fn extended_euclidean_algorithm(a: i32, b: i32) -> (i32, i32, i32) {
-    let (mut old_r, mut rem) = (a, b);
-    let (mut old_s, mut coeff_s) = (1, 0);
-    let (mut old_t, mut coeff_t) = (0, 1);
-
-    while rem != 0 {
-        let quotient = old_r / rem;
-
-        update_step(&mut rem, &mut old_r, quotient);
-        update_step(&mut coeff_s, &mut old_s, quotient);
-        update_step(&mut coeff_t, &mut old_t, quotient);
-    }
-
-    (old_r, old_s, old_t)
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let t = Instant::now();
 
@@ -50,114 +28,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         let button_b = i64vec2(button_b[0], button_b[1]);
         let prize = i64vec2(prize[0] + 10000000000000, prize[1] + 10000000000000);
 
-        println!("button_a {button_a}, button_b {button_b}");
-
-        println!("A * {} + B * {} = {}", button_a.x, button_b.x, prize.x);
-        println!("A * {} + B * {} = {}", button_a.y, button_b.y, prize.y);
-
-        println!(
-            "A * {} + B * {} = {}",
-            button_a.x * button_a.y,
-            button_b.x * button_a.y,
-            prize.x * button_a.y
-        );
-        println!(
-            "A * {} + B * {} = {}",
-            button_a.y * button_a.x,
-            button_b.y * button_a.x,
-            prize.y * button_a.x
-        );
-
-        println!(
-            "A * {} = {} - B * {}",
-            button_a.x * button_a.y,
-            prize.x * button_a.y,
-            button_b.x * button_a.y
-        );
-        println!(
-            "A * {} = {} - B * {}",
-            button_a.y * button_a.x,
-            prize.y * button_a.x,
-            button_b.y * button_a.x
-        );
-
-        println!(
-            "{} - B * {} = {} - B * {}",
-            prize.x * button_a.y,
-            button_b.x * button_a.y,
-            prize.y * button_a.x,
-            button_b.y * button_a.x
-        );
-
-        println!(
-            "{} - {} =  - B * {} + B * {}",
-            prize.x * button_a.y,
-            prize.y * button_a.x,
-            button_b.y * button_a.x,
-            button_b.x * button_a.y
-        );
-
-        println!(
-            "{} = B * ({} - {})",
-            prize.x * button_a.y - prize.y * button_a.x,
-            button_b.x * button_a.y,
-            button_b.y * button_a.x
-        );
-
-        println!(
-            "{} = B * {}",
-            prize.x * button_a.y - prize.y * button_a.x,
-            button_b.x * button_a.y - button_b.y * button_a.x
-        );
-
         let left = prize.x * button_a.y - prize.y * button_a.x;
         let right = button_b.x * button_a.y - button_b.y * button_a.x;
 
-        if left % right != 0 {
-            println!("no good");
-        } else {
+        if left % right == 0 {
             let b = left / right;
-            println!("B = {b}");
-
-            println!(
-                "A * {} + {b} * {} = {}",
-                button_a.y * button_a.x,
-                button_b.y * button_a.x,
-                prize.y * button_a.x
-            );
-
-            println!(
-                "A * {} = {} - {b} * {}",
-                button_a.y * button_a.x,
-                prize.y * button_a.x,
-                button_b.y * button_a.x
-            );
-
-            println!(
-                "A * {} = {} - {}",
-                button_a.y * button_a.x,
-                prize.y * button_a.x,
-                b * button_b.y * button_a.x
-            );
-
-            println!(
-                "A * {} = {}",
-                button_a.y * button_a.x,
-                prize.y * button_a.x - b * button_b.y * button_a.x
-            );
 
             let left = button_a.y * button_a.x;
             let right = prize.y * button_a.x - b * button_b.y * button_a.x;
 
-            if right % left != 0 {
-                println!("NO GOOD");
-            } else {
+            if right % left == 0 {
                 let a = right / left;
                 res += a * 3 + b;
             }
         }
-
-        println!();
     }
 
     println!("res: {res}, {} us", t.elapsed().as_micros());
