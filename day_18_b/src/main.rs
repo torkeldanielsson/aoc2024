e9dfeff8fd73e8019e6552ed9324aff5c7a3e172
torkel <torkel@voysys.se>
2024-12-18 09:30:42 +0100

    18b binary search

--- day_18_b/src/main.rs
+++ day_18_b/src/main.rs
@@ -27,12 +27,62 @@ impl PartialOrd for Pos {
     }
 }
 
+fn can_solve_maze(all_obstacles: &[IVec2], obstacle_count: usize, goal: IVec2) -> bool {
+    let mut obstacles = FxHashSet::with_capacity_and_hasher(5000, Default::default());
+
+    for o in all_obstacles.iter().take(obstacle_count) {
+        obstacles.insert(*o);
+    }
+
+    let mut open_set = BinaryHeap::new();
+    open_set.push(Pos::new(goal.x + goal.y, ivec2(0, 0)));
+
+    let mut came_from = FxHashMap::with_capacity_and_hasher(5000, Default::default());
+
+    let mut g_score = FxHashMap::with_capacity_and_hasher(5000, Default::default());
+    g_score.insert(ivec2(0, 0), 0);
+
+    let mut f_score = FxHashMap::with_capacity_and_hasher(5000, Default::default());
+    f_score.insert(ivec2(0, 0), goal.x + goal.y);
+
+    while let Some(current) = open_set.pop() {
+        if current.pos == goal {
+            return true;
+        }
+
+        for neighbor in &[ivec2(1, 0), ivec2(-1, 0), ivec2(0, 1), ivec2(0, -1)] {
+            let neighbor = current.pos + neighbor;
+
+            if obstacles.contains(&neighbor)
+                || neighbor.x < 0
+                || neighbor.y < 0
+                || neighbor.x > goal.x
+                || neighbor.y > goal.y
+            {
+                continue;
+            }
+
+            let tentative_g_score = g_score[&current.pos] + 1;
+
+            if tentative_g_score < *g_score.entry(neighbor).or_insert(i32::MAX) {
+                came_from.insert(neighbor, current.pos);
+                g_score.insert(neighbor, tentative_g_score);
+                let neighbor_f_score =
+                    tentative_g_score + goal.x - neighbor.x + goal.y - neighbor.y;
+                f_score.insert(neighbor, neighbor_f_score);
+                open_set.push(Pos::new(neighbor_f_score, neighbor));
+            }
+        }
+    }
+    false
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let t = Instant::now();
 
     let input = include_str!("../input");
 
-    let mut all_obstacles = Vec::new();
+    let mut all_obstacles = Vec::with_capacity(5000);
 
     let mut goal = ivec2(0, 0);
 
@@ -43,75 +93,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         goal.y = goal.y.max(nums[1]);
     }
 
-    let mut obstacles = FxHashSet::with_capacity_and_hasher(50000, Default::default());
-
     let mut obstacle_count = 1024;
+    let mut half_remainder = (all_obstacles.len() - obstacle_count) / 2;
 
-    for o in all_obstacles.iter().take(obstacle_count) {
-        obstacles.insert(*o);
-    }
+    loop {
+        let solvable = can_solve_maze(&all_obstacles, obstacle_count, goal);
 
-    'a_star_loop: loop {
-        obstacles.insert(all_obstacles[obstacle_count]);
-        obstacle_count += 1;
-
-        let mut open_set = BinaryHeap::new();
-        open_set.push(Pos::new(goal.x + goal.y, ivec2(0, 0)));
-
-        let mut came_from = FxHashMap::with_capacity_and_hasher(50000, Default::default());
-
-        let mut g_score = FxHashMap::with_capacity_and_hasher(50000, Default::default());
-        g_score.insert(ivec2(0, 0), 0);
-
-        let mut f_score = FxHashMap::with_capacity_and_hasher(50000, Default::default());
-        f_score.insert(ivec2(0, 0), goal.x + goal.y);
-
-        while let Some(current) = open_set.pop() {
-            if current.pos == goal {
-                continue 'a_star_loop;
-            }
-
-            for neighbor in &[ivec2(1, 0), ivec2(-1, 0), ivec2(0, 1), ivec2(0, -1)] {
-                let neighbor = current.pos + neighbor;
-
-                if obstacles.contains(&neighbor)
-                    || neighbor.x < 0
-                    || neighbor.y < 0
-                    || neighbor.x > goal.x
-                    || neighbor.y > goal.y
-                {
-                    continue;
-                }
-
-                let tentative_g_score = g_score[&current.pos] + 1;
-
-                if tentative_g_score < *g_score.entry(neighbor).or_insert(i32::MAX) {
-                    came_from.insert(neighbor, current.pos);
-                    g_score.insert(neighbor, tentative_g_score);
-                    let neighbor_f_score =
-                        tentative_g_score + goal.x - neighbor.x + goal.y - neighbor.y;
-                    f_score.insert(neighbor, neighbor_f_score);
-                    open_set.push(Pos::new(neighbor_f_score, neighbor));
-                }
-            }
+        if solvable {
+            obstacle_count += half_remainder;
+        } else {
+            obstacle_count -= half_remainder;
         }
 
-        break;
-    }
+        half_remainder /= 2;
 
-    // for y in 0..=goal.y {
-    //     for x in 0..=goal.x {
-    //         if obstacles.contains(&ivec2(x, y)) {
-    //             print!("#")
-    //         } else if path_back.contains(&ivec2(x, y)) {
-    //             print!("O")
-    //         } else {
-    //             print!(".")
-    //         }
-    //     }
-    //     println!();
-    // }
-    // println!();
+        if half_remainder < 1 {
+            break;
+        }
+    }
 
     println!(
         "res: {},{}, {} us",
