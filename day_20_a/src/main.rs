fa4b557bb3fffa1786e384b0ad0a8f28766cddbd
torkel <torkel@voysys.se>
2024-12-20 09:15:40 +0100

    20 a

--- day_20_a/src/main.rs
+++ day_20_a/src/main.rs
@@ -1,31 +1,7 @@
-use std::{collections::BinaryHeap, error::Error, time::Instant};
+use std::{error::Error, time::Instant};
 
-use glam::{ivec2, IVec2};
+use glam::ivec2;
 use rustc_hash::{FxHashMap, FxHashSet};
-
-#[derive(Eq, PartialEq)]
-struct Pos {
-    f_score: i32,
-    pos: IVec2,
-}
-
-impl Pos {
-    fn new(f_score: i32, pos: IVec2) -> Self {
-        Pos { f_score, pos }
-    }
-}
-
-impl Ord for Pos {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.f_score.cmp(&self.f_score)
-    }
-}
-
-impl PartialOrd for Pos {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let t = Instant::now();
@@ -52,68 +28,46 @@ fn main() -> Result<(), Box<dyn Error>> {
         max_map.y = max_map.y.max(y as i32);
     }
 
-    let mut open_set = BinaryHeap::new();
-    open_set.push(Pos::new(goal.x + goal.y, start));
+    let mut prev = ivec2(-1, -1);
+    let mut p = start;
+    let mut steps = 0;
 
-    let mut came_from = FxHashMap::with_capacity_and_hasher(5000, Default::default());
+    let mut time_track = FxHashMap::default();
+    time_track.insert(start, 0);
 
-    let mut g_score = FxHashMap::with_capacity_and_hasher(5000, Default::default());
-    g_score.insert(start, 0);
-
-    let mut f_score = FxHashMap::with_capacity_and_hasher(5000, Default::default());
-    f_score.insert(start, goal.x + goal.y);
-
-    while let Some(current) = open_set.pop() {
-        if current.pos == goal {
-            break;
-        }
-
-        // for y in 0..=max_map.y {
-        //     for x in 0..=max_map.x {
-        //         let p = ivec2(x, y);
-        //         if obstacles.contains(&p) {
-        //             print!("#")
-        //         } else if current.pos == p {
-        //             print!("O")
-        //         } else {
-        //             print!(".")
-        //         }
-        //     }
-        //     println!();
-        // }
-        // println!();
-        // println!();
-
+    while p != goal {
         for neighbor in &[ivec2(1, 0), ivec2(-1, 0), ivec2(0, 1), ivec2(0, -1)] {
-            let neighbor = current.pos + neighbor;
+            let neighbor = p + neighbor;
 
-            if obstacles.contains(&neighbor)
-                || neighbor.x < 0
-                || neighbor.y < 0
-                || neighbor.x > max_map.x
-                || neighbor.y > max_map.y
-            {
-                continue;
-            }
-
-            let tentative_g_score = g_score[&current.pos] + 1;
-
-            if tentative_g_score < *g_score.entry(neighbor).or_insert(i32::MAX) {
-                came_from.insert(neighbor, current.pos);
-                g_score.insert(neighbor, tentative_g_score);
-                let neighbor_f_score =
-                    tentative_g_score + goal.x - neighbor.x + goal.y - neighbor.y;
-                f_score.insert(neighbor, neighbor_f_score);
-                open_set.push(Pos::new(neighbor_f_score, neighbor));
+            if !obstacles.contains(&neighbor) && neighbor != prev {
+                prev = p;
+                p = neighbor;
+                steps += 1;
+                time_track.insert(p, steps);
             }
         }
     }
 
-    println!(
-        "g_score goal: {}, {} us",
-        g_score[&goal],
-        t.elapsed().as_micros()
-    );
+    let mut res = 0;
+
+    for (start_pos, start_score) in &time_track {
+        for neighbor in &[ivec2(1, 0), ivec2(-1, 0), ivec2(0, 1), ivec2(0, -1)] {
+            let neighbor_1 = start_pos + neighbor;
+            let neighbor_2 = start_pos + neighbor * 2;
+
+            if obstacles.contains(&neighbor_1)
+                && time_track.contains_key(&neighbor_2)
+                && time_track[&neighbor_2] > start_score + 2
+            {
+                let cheat_score = time_track[&neighbor_2] - (start_score + 2);
+                if cheat_score >= 100 {
+                    res += 1;
+                }
+            }
+        }
+    }
+
+    println!("res: {res}, {} us", t.elapsed().as_micros());
 
     Ok(())
 }
